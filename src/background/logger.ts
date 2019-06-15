be3c2f3497b22b8618ac0629f4dd19aff73487fa
LeoMartinDev <leo1.martin@epitech.eu>
2019-06-15 14:20:27 +0200

    open logs file & log padding

--- src/background/logger.ts
+++ src/background/logger.ts
@@ -2,7 +2,7 @@ import log, { IElectronLog, ITransport, IConsoleTransport } from 'electron-log';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const format = '[MAIN] :: [{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
+const format = '[MAIN]     :: [{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
 
 log.transports.file.format = format;
 log.transports.console.format = format;
